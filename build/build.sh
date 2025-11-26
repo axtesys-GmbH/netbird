8a798bd0d88d150848029a2445d9d432528b872c
Thomas Neumann <thomas.neumann@axtesys.at>
2025-11-26 16:28:44 +0100

    Run releaser as container to mount docker

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 export DOCKER_BUILDKIT=1
+# Ensure QEMU emulation is available for cross-platform builds
+docker run --privileged --rm tonistiigi/binfmt --install all
 
-#rm -rf dist && mkdir dist
+rm -rf dist && mkdir dist
 docker build --force-rm -t local/netbird-releaser -f build/Base.Dockerfile --target=builder .
+docker run --rm -v "/var/run/docker.sock:/var/run/docker.sock" -v "$(pwd)/dist:/app/dist" local/netbird-releaser --skip=publish,announce
 #docker build --force-rm -t local/netbird-releaser -f build/Base.Dockerfile --output=./dist .
-#docker build --force-rm -t local/netbird-releaser-ui -f build/UI.Dockerfile --output=./dist .
-#docker build --force-rm -t local/netbird-releaser-ui -f build/UI_Darwin.Dockerfile --output=./dist .
-#docker build --force-rm -t local/netbird-releaser-wininstall -f build/WinInstaller.Dockerfile --output=./dist .
+docker build --force-rm -t local/netbird-releaser-ui -f build/UI.Dockerfile --output=./dist .
+docker build --force-rm -t local/netbird-releaser-ui -f build/UI_Darwin.Dockerfile --output=./dist .
+docker build --force-rm -t local/netbird-releaser-wininstall -f build/WinInstaller.Dockerfile --output=./dist .
